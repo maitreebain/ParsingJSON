3730385bea77b86bf9677086298c17a65389d220
maitreebain <maitreebain@gmail.com>
2020-07-28 12:24:38 -0400

    successfully parsed contacts dictionary

--- JSONPractice.playground/Pages/ParsingDictionary.xcplaygroundpage/Contents.swift
+++ JSONPractice.playground/Pages/ParsingDictionary.xcplaygroundpage/Contents.swift
@@ -1,1 +1,42 @@
-//parsing Dictionary
+//Parsing Dictionary
+import Foundation
+
+let json = """
+{
+ "results": [
+   {
+     "firstName": "John",
+     "lastName": "Appleseed"
+   },
+  {
+    "firstName": "Alex",
+    "lastName": "Paul"
+  }
+ ]
+}
+""".data(using: .utf8)!
+
+//Create models
+//Codable is an alias (Decodable & Encodable)
+//Decodable: Converts JSON data
+//Encodable: Converts JSON data to e.g Web API
+struct ResultWrapper: Decodable {
+    let results: [Contact]
+}
+
+struct Contact: Decodable {
+    let firstName: String
+    let lastName: String
+}
+
+/*
+ Decode the JSON data to our Swift Model
+ */
+
+do {
+    let dictionary = try JSONDecoder().decode(ResultWrapper.self, from: json)
+    let contacts = dictionary.results
+    dump(contacts)
+} catch {
+    print("decoding error: \(error)")
+}
